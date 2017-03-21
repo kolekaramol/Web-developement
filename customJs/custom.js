4c8f91e7f122acb379db7475239ce76cb866ffb2
Kolekaramol <kolekaramol@hotmail.com>
2017-03-21 14:17:18 +0530

    addede file

---
+++ customJs/custom.js
@@ -0,0 +1,17 @@
+function sum () {
+	alert('hi');
+	var x=document.getElementById('value1').value;
+	var y=document.getElementById('value2').value;
+	alert(x+y);
+}
+
+/*var sum1=sum();
+alert('sum ='+sum1);
+*/
+function sum(x,y) 
+{
+	return x+y;
+}
+
+/*var result=sum(12,23);
+alert("result ="+result);*/
